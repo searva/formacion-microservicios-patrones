5e23275cd5403330376f9034d8eb9ccaeccaf2d1
SERGIO ARANDA VAZQUEZ <0003805@insags.com>
2018-10-11 09:48:34 +0200

    incluyo comentario del uso de @EnableOAuth2Sso

--- spring-security-sso-ui/src/main/java/com/in28minutes/microservices/springsecurityssoui/UiSecurityConfig.java
+++ spring-security-sso-ui/src/main/java/com/in28minutes/microservices/springsecurityssoui/UiSecurityConfig.java
@@ -11,6 +11,22 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+/** 
+ * The main goal of @EnableOAuth2Sso is to allow user's to authenticate to the application using an external OAuth 2.0 provider. 
+ * This is a very convenient authentication option an application can provide as it doesn't need to provide it's own authentication mechanism 
+ * (e.g. httpSecurity.formLogin()) and instead leverages an external identity (OAuth 2.0) provider for authentication.
+ *
+ * The @EnableOAuth2Sso annotation registers the OAuth2ClientAuthenticationProcessingFilter, which uses a RestTemplate (the OAuth Client) 
+ * to obtain an Access Token from the Provider. After the Client receives the Access Token from the Provider, it would typically call the 
+ * UserInfo Endpoint to fetch user information in order to create the Authentication. However, since you are using JWT based tokens, the 
+ * DefaultAccessTokenConverter.extractAuthentication() is called (used by JwtAccessTokenConverter) to extract the information 
+ * (using user_name and authorities claims) to create the OAuth2Authentication and establish an authenticated session. The Access Token 
+ * used to obtain the user information is used once only during authentication. After the user is authenticated, that Access Token is never 
+ * used again. Moreover, the refresh_token grant is not applicable for this specific OAuth Client scenario (flow).
+ * 
+ * 
+ * **/
+
 @EnableOAuth2Sso
 @Configuration
 public class UiSecurityConfig extends WebSecurityConfigurerAdapter {
